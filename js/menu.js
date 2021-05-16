445be39556e14771c305799dcb6b0581d565901c
pedropurihuaman <purihuamanpurihuamanpedro@gmail.com>
2021-05-16 00:31:31 -0500

    Creamos un menu responsive con flexbox

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+const toggleMenu = document.getElementById('toggle-menu');
+const menu = document.getElementById('menu');
+
+
+toggleMenu.addEventListener('click', () => {
+	menu.classList.toggle('menu--show');
+});
